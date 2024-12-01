0609b96821b087e0aa88f63cd7b80e3fb85f97e5
Crowdedlight <crowdedhd@gmail.com>
2024-12-01 20:42:58 +0100

    day1

--- day01/src/part1.rs
+++ day01/src/part1.rs
@@ -1,5 +1,22 @@
 pub fn process(input: &str) -> anyhow::Result<String> {
-    todo!()
+
+    let mut a_list: Vec<u32> = vec![];
+    let mut b_list: Vec<u32> = vec![];
+
+    // split into a and b lists
+    for line in input.lines() {
+        let (a, b) = line.split_once(' ').unwrap();
+        a_list.push(a.trim().parse()?);
+        b_list.push(b.trim().parse()?);
+    }
+
+    // sort both in increasing order
+    a_list.sort();
+    b_list.sort();
+
+    // can we just subtract each list with eachother with abs() and sum the resulting list?
+    let res: Vec<u32> = a_list.into_iter().zip(b_list).map(|(a, b)| a.abs_diff(b)).collect();
+    Ok(res.iter().sum::<u32>().to_string())
 }
 
 #[cfg(test)]
@@ -8,17 +25,13 @@ mod tests {
 
     #[test]
     fn test_process() -> anyhow::Result<()> {
-        let input = "467..114..
-        ...*......
-        ..35..633.
-        ......#...
-        617*......
-        .....+.58.
-        ..592.....
-        ......755.
-        ...$.*....
-        .664.598..";
-                assert_eq!("4361", process(input)?);
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+                assert_eq!("11", process(input)?);
                 Ok(())
     }
 }
--- day01/src/part2.rs
+++ day01/src/part2.rs
@@ -1,5 +1,30 @@
+use std::collections::HashMap;
+
 pub fn process(input: &str) -> anyhow::Result<String> {
-    todo!()
+
+    let mut a_list: Vec<u32> = vec![];
+    // hashmap makes more sense for this problem
+    let mut map: HashMap<u32, u32> = HashMap::new();
+
+    // split into a and b lists
+    for line in input.lines() {
+        let (a, b) = line.split_once(' ').unwrap();
+        a_list.push(a.trim().parse()?);
+
+        // get or create key
+        let b_val = b.trim().parse()?;
+        map.entry(b_val).and_modify(|v| *v += 1).or_insert(1);
+    }
+
+    // make right list a hashmap with the number as key and the value the count
+    let mut sum: u64 = 0;
+    for val in a_list {
+        // get count for key in map, if key doesn't exist, we return 0
+        let count = map.get(&val).unwrap_or(&0);
+        sum += *count as u64 * val as u64;
+    }
+    
+    Ok(sum.to_string())
 }
 
 #[cfg(test)]
@@ -8,17 +33,13 @@ mod tests {
 
     #[test]
     fn test_process() -> anyhow::Result<()> {
-        let input = "467..114..
-        ...*......
-        ..35..633.
-        ......#...
-        617*......
-        .....+.58.
-        ..592.....
-        ......755.
-        ...$.*....
-        .664.598..";
-                assert_eq!("4361", process(input)?);
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+                assert_eq!("31", process(input)?);
                 Ok(())
     }
 }
